53d71974fe56f1d04d49a61f6ecfa57961dbd19e
Kermah01 <126328483+Kermah01@users.noreply.github.com>
2023-11-24 12:29:00 +0000

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -65,6 +65,16 @@ right: 2rem;
 }}
 </style>
 """
+st.markdown(
+    """
+    <style>
+        body {
+            color: white;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 st.markdown('<div style="text-align:center;width:185%;"><h1 style="color:white;background-color:black;border:red;border-style:solid;border-radius:5px;">TABLEAU DE BORD INTERACTIF</h1></div>', unsafe_allow_html=True)
 
 colors = px.colors.sequential.Rainbow_r
